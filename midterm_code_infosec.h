d7d6e70e34320d53952fce691cc6fe12483db14c
trigeekm <test@aol.com>
2017-03-01 21:55:34 -0500

    Midterm Project for Information Security
    This is the midterm project for my group and I to work on.

---
+++ midterm_code_infosec.h
@@ -0,0 +1,12 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string name;
+  cout << "What is your name? ";
+  getline (cin, name);
+  cout << "Hello, " << name << "!\n";
+}
