ee53425a20e057beb600c880d229b265540efd2c
sapfel93 <sanfordapfel@yahoo.com>
2017-03-03 17:41:42 -0500

    Update midterm_code_infosec.h

--- midterm_code_infosec.h
+++ midterm_code_infosec.h
@@ -11,17 +11,19 @@ int main()
 	getline(cin, name);
 	cout << "How old are you?";  // Sanford Apfel
 	std::cin >> age;
-	cout << "Hello, " << name << ", You are " << age << " years old"!\n" << endl;  // John Von Hagen
-		"Will you be getting the Nintendo Switch? (1: Yes , 2: No , 3: Maybe)" << endl;  // Dyana Jo Stewart
+	cout << "Hello, " << name << ", You are " << age << " years old!\n"// John Von Hagen
+	        "Will you be getting the Nintendo Switch? (1: Yes , 2: No , 3: Maybe)" << endl;  // Dyana Jo Stewart
 	std::cin >> switchans;  // Dyana Jo Stewart
-		switch (switchans) {
-		case 1: std::cout << "Enjoy spending $70 on a new controller.\n";
-			break;
-		case 2: std::cout << "I guess you'll be missing out on BOTW!\n";
-			break;
-			case 3: std::cout << "You really should commit one way or another.\n";					
-		default: std::cout << "Invalid Message.";
-			break;
+		switch (switchans) 
+		{
+			case 1: std::cout << "Enjoy spending $70 on a new controller.\n";
+				break;
+			case 2: std::cout << "I guess you'll be missing out on BOTW!\n";
+				break;
+			case 3: std::cout << "You really should commit one way or another.\n";	
+				break;
+			default: std::cout << "Invalid Message.";
+				break;
 		}
 		return 0;
 }
