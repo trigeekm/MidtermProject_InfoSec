5baa857e3b760517faed1c5262b8381bc555e03e
Dyana Jo Stewart <Dyanajo@bellsouth.net>
2017-03-02 21:14:44 -0500

    Update midterm_code_infosec.h

--- midterm_code_infosec.h
+++ midterm_code_infosec.h
@@ -12,13 +12,14 @@ int main()
 	cout << "How old are you?";  // Sanford Apfel
 	std::cin >> age;
 	cout << "Hello, " << name << ", You are " << age << " years old"!\n" << endl;  // John Von Hagen
-		"Will you be getting the Nintendo Switch? (1: Yes , 2: No)" << endl;  // Dyana Jo Stewart
+		"Will you be getting the Nintendo Switch? (1: Yes , 2: No , 3: Maybe)" << endl;  // Dyana Jo Stewart
 	std::cin >> switchans;  // Dyana Jo Stewart
 		switch (switchans) {
 		case 1: std::cout << "Enjoy spending $70 on a new controller.\n";
 			break;
 		case 2: std::cout << "I guess you'll be missing out on BOTW!\n";
 			break;
+			case 3: std::cout << "You really should commit one way or another.\n";					
 		default: std::cout << "Invalid Message.";
 			break;
 		}
